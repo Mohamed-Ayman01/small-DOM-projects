b88097b2013ce800e788ac7c81cea67c1eed12de
Mohamed-Ayman01 <lord39597@gmail.com>
2021-09-27 19:34:23 +0400

    file renamed

--- Slider - carousal/main.js
+++
@@ -1,8 +0,0 @@
-let toggleBtn = document.querySelector(".switch");
-
-toggleBtn.addEventListener('click', () => {
-  document.body.classList.toggle("dark");
-  document.body.classList.contains("dark")
-    ? toggleBtn.innerHTML = "Light Mode"
-    : toggleBtn.innerHTML = "Dark Mode"
-})
