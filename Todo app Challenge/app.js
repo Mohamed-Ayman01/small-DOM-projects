463605bd0142136859995dd3abf6bb35d90e6a5e
Mohamed-Ayman01 <lord39597@gmail.com>
2021-10-21 18:50:32 +0200

    code improve (enhancement)

--- Todo app Challenge/app.js
+++ Todo app Challenge/app.js
@@ -2,57 +2,95 @@ const taskInput = document.getElementById("task");
 const submitTaskBtn = document.getElementById("sub-task");
 const tasksContainer = document.querySelector(".tasks-container");
 
+// Check If There is tasks in LocalStorage
+function checkForTask() {
+  if (window.localStorage.length >= 1) {
+    // remove no tasks message
+    if (document.querySelector(".tasks-container h3") !== null) {
+      document.querySelector(".tasks-container h3").remove();
+    }
+
+    // Loop on tasks to add them
+    for (let i = 0; i < window.localStorage.length; i++) {
+      if (window.localStorage.getItem(`Task-No.${i}`)) {
+        // Create Element and Set attributes
+        let taskBox = document.createElement("div");
+        let paragraph = document.createElement("p");
+        let delTaskBtn = document.createElement("button");
+        let paragrapghText = document.createTextNode(window.localStorage.getItem(`Task-No.${i}`));
+
+        taskBox.classList.add("task", `Task-No.${i}`);
+        delTaskBtn.classList.add("del-task");
+        delTaskBtn.innerText = "Remove";
+        paragraph.append(paragrapghText);
+
+        taskBox.append(paragraph, delTaskBtn);
+
+        tasksContainer.prepend(taskBox);
+      } else continue;
+    }
+  } else {
+    if ((document.querySelector(".tasks-container h3") === null)) {
+      let messageH3 = document.createElement("h3");
+      messageH3.innerText = "There are no Tasks Yet";
+      tasksContainer.append(messageH3);
+    } 
+  }
+}
+checkForTask()
+
 // Hold input field value
 let taskVal;
 taskInput.oninput = () => (taskVal = taskInput.value);
 
-// Check If There is tasks in LocalStorage
-if (window.localStorage.length >= 1) {
-  for (let i = 0; i < window.localStorage.length; i++) {
-    if (window.localStorage.getItem(`Task-No.${i}`)) {
-      // Create Element and Set attributes
-      let taskBox = document.createElement("div");
-      let paragraph = document.createElement("p");
-      let delTaskBtn = document.createElement("button");
-      let paragrapghText = document.createTextNode(window.localStorage.getItem(`Task-No.${i}`));
+// popup (modal)
 
-      taskBox.classList.add("task", `Task-No.${i}`);
-      delTaskBtn.classList.add("del-task");
-      delTaskBtn.innerText = "Remove";
-      paragraph.append(paragrapghText);
+let popup = document.querySelector(".popup");
+let modalExitBtn = document.querySelector(".popup .exit");
+let overlay = document.querySelector(".popup-overlay")
+modalExitBtn.onclick = () => {
+  popup.classList.remove("show")
+  overlay.style.opacity = "0";
 
-      taskBox.append(paragraph, delTaskBtn);
-
-      tasksContainer.prepend(taskBox);
-    } else continue;
-  }
-} else {
-  tasksContainer.innerText = "There are no Tasks Yet ..";
+  setTimeout(() => {
+    popup.style.setProperty("display", "none")
+    overlay.style.setProperty("display", "none")
+  }, 200)
 }
 
 // Add New Task
-submitTaskBtn.onclick = addNewTask;
-
 function addNewTask() {
   if (taskVal !== "" && taskVal !== undefined) {
     // Save Current Task Title to Local Storage
     window.localStorage.setItem(`Task-No.${window.localStorage.length}`, taskVal);
 
-    // Relod Window To Refresh Tasks And clear input
-    window.location.reload();
+    // clear last value & clear task container
+    tasksContainer.innerHTML = "";
+    taskInput.value = "";
+    taskVal = "";
   } else {
-    window.alert("Please Enter A Valid Task Title")
+    popup.style.setProperty("display", "block");
+    overlay.style.setProperty("display", "block");
+
+    setTimeout(() => {
+      popup.classList.add("show");
+      overlay.style.opacity = "1";
+    }, 200)
   }
 }
+submitTaskBtn.addEventListener("click",  addNewTask);
+// Add task to tasks container using => checkForTask() func
+submitTaskBtn.addEventListener("click",  checkForTask);
 
+// Remove done Task
 function RemoveCurrentTask() {
-  document.addEventListener('click', (e) => {
+  document.addEventListener("click", (e) => {
     if (e.target.classList.contains("del-task")) {
       // Remove Task Data from local storage
       window.localStorage.removeItem(`${e.target.parentElement.classList.item(1)}`);
       // remove task Box HTML element
       e.target.parentElement.remove();
     }
-  })
+  });
 }
 RemoveCurrentTask();
