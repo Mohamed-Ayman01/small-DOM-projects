9f91f6c04c85e6ae351f1bc052406ef687a17f19
Mohamed-Ayman01 <lord39597@gmail.com>
2021-10-30 18:59:50 +0200

    bullet based carousal/slider

---
+++ bullets (slider-carousal)/main.js
@@ -0,0 +1,43 @@
+let displayedImage = document.querySelector(".displayed-image");
+let bullets = document.querySelectorAll(".bullets-box .bullet");
+
+let index = 0;
+
+function iterateOnImages() {
+  index += 1;
+
+  if (index >= bullets.length) index = 0;
+
+  bullets.forEach((el) => el.classList.remove("active"));
+  bullets[index].classList.add("active");
+
+  displayedImage.style.cssText = "opacity: 0; transform: scale(1.5)";
+  
+  setTimeout(() => {
+    displayedImage.src = `./imgs/${bullets[index].dataset.path}`;
+  }, 300);
+  
+  setTimeout(() => {
+    displayedImage.style.cssText = "opacity: 1; transform: scale(1)";
+  }, 350);
+}
+
+setInterval(iterateOnImages, 5000);
+
+bullets.forEach((span) => {
+  span.addEventListener("click", function () {
+    index = Array.from(bullets).indexOf(span);
+    
+    bullets.forEach((el) => el.classList.remove("active"));
+    span.classList.add("active");
+    
+    displayedImage.style.cssText = "opacity: 0; transform: scale(1.5)";
+    setTimeout(() => {
+      displayedImage.src = `./imgs/${this.dataset.path}`;
+    }, 300);
+    
+    setTimeout(() => {
+      displayedImage.style.cssText = "opacity: 1; transform: scale(1)";
+    }, 350);
+  });
+});
